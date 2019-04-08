63d79ee8c330f1099647d6f2cd077684fdfae445
Farry <yu@iotserv.com>
2019-04-08 16:55:45 +0800

    从配置文件加载配置

--- config/config.go
+++ config/config.go
@@ -5,16 +5,30 @@ import (
 	"git.iotserv.com/iotserv/utils/models"
 	"gopkg.in/yaml.v2"
 	"io/ioutil"
+	"os"
 )
 
 var ConfigMode models.ServerConfig
 
 func init() {
 	var err error
-	ConfigMode, err = GetConfig("./server.yaml")
+	var configFilePath = "./server.yaml"
+	_, err = os.Stat(configFilePath)
+	if err != nil {
+		fmt.Println("没有找到配置文件：", configFilePath)
+		fmt.Println("开始生成默认的空白配置文件，请填写配置文件后重复运行本程序")
+		//	生成配置文件模板
+		err = writeConfigFile(ConfigMode, configFilePath)
+		if err != nil {
+			fmt.Println(err.Error())
+			os.Exit(1)
+		}
+		os.Exit(1)
+	}
+	ConfigMode, err = GetConfig(configFilePath)
 	if err != nil {
 		fmt.Printf(err.Error())
-		return
+		os.Exit(1)
 	}
 }
 
@@ -27,6 +41,19 @@ func GetConfig(configFilePath string) (configMode models.ServerConfig, err error
 	err = yaml.Unmarshal(content, &configMode)
 	if err != nil {
 		fmt.Println(err.Error())
+		return
+	}
+	return
+}
+
+func writeConfigFile(configMode models.ServerConfig, path string) (err error) {
+	configByte, err := yaml.Marshal(configMode)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	if ioutil.WriteFile(path, configByte, 0644) == nil {
+		fmt.Println("写入配置文件文件成功！\n")
 		return
 	}
 	return
