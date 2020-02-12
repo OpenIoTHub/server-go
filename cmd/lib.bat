b45f6d9b1b293abefcd340c1d80bef93cef4876b
Farry <newfarry@126.com>
2020-02-12 14:49:38 +0800

    结构优化

--- cmd/lib.bat
+++
@@ -1,9 +0,0 @@
-set CGO_ENABLED=1
-set GOROOT_BOOTSTRAP=C:/Go
-::x86块
-set GOARCH=386
-set GOOS=linux
-go build -buildmode=c-shared -o libiotserv.so main.go
-set GOARCH=amd64
-set GOOS=windows
-pause
--- session/servTlsLogin.go
+++ session/servTlsLogin.go
@@ -3,7 +3,7 @@ package session
 import (
 	"crypto/tls"
 	"fmt"
-	"github.com/OpenIoTHub/Server/config"
+	"github.com/OpenIoTHub/server-go/config"
 	"os"
 )
 
