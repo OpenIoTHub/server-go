7db73f71bcb72070267fe4f048539a946edbc820
Farry <newfarry@126.com>
2020-02-14 17:34:09 +0800

    优化配置文件加载

--- main.go
+++ main.go
@@ -5,16 +5,30 @@ import (
 	"github.com/OpenIoTHub/server-go/config"
 	"github.com/OpenIoTHub/server-go/session"
 	"github.com/OpenIoTHub/server-go/udpapi"
+	"os"
 	"time"
 )
 
 func main() {
+	err := run()
+	if err != nil {
+		os.Exit(1)
+	}
+	for {
+		time.Sleep(time.Hour * 99999)
+	}
+}
+
+func run() (err error) {
+	err = config.LoadConfig()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 	go session.RunTLS(config.ConfigMode.Common.TlsPort)
 	go session.RunTCP(config.ConfigMode.Common.TcpPort)
 	go session.RunKCP(config.ConfigMode.Common.KcpPort)
 	go udpapi.RunApiServer(config.ConfigMode.Common.UdpApiPort)
 	fmt.Println("服务器正在运行，内网端配置请根据本服务器配置填写！")
-	for {
-		time.Sleep(time.Hour * 99999)
-	}
+	return
 }
