d41ca81d2333e40a5387ab0a75caa7da1e272b5f
Farry <newfarry@126.com>
2020-04-22 22:58:28 +0800

    update:扩充

--- session/SessionsManager.go
+++ session/SessionsManager.go
@@ -191,34 +191,3 @@ func (sess SessionsManager) openIoTHubLoginHdl(id string, conn net.Conn) {
 		return
 	}
 }
-
-//
-type Session struct {
-	Id             string
-	Conn           *net.Conn
-	GatewaySession *mux.Session
-	WorkConn       chan net.Conn
-}
-
-//:TODO 存活检测
-func (sess *Session) Task() {
-	//defer DelSession(sess.Id)
-	//Loop:
-	//for {
-	//	select {
-	//		case <-sess.heartbeat.C:
-	//			stream,err:=sess.GatewaySession.OpenStream()
-	//			if err != nil{
-	//				fmt.Printf(err.Error())
-	//				break Loop
-	//			}
-	//			err=msg.WriteMsg(stream, &models.Ping{})
-	//			if err != nil{
-	//				fmt.Printf(err.Error())
-	//				break Loop
-	//			}
-	//			stream.Close()
-	//		}
-	//}
-	fmt.Printf("end session Task")
-}
