1425b863c1108477855df44a3ac7db2f486a4634
Farry <newfarry@126.com>
2020-05-12 22:58:54 +0800

    update grpc

--- config/constants.go
+++ config/constants.go
@@ -4,14 +4,15 @@ var DefaultConfigFileName = "server.yaml"
 var DefaultConfigFilePath = "./server.yaml"
 
 var DefaultBindAddr = "0.0.0.0"
+
 var DefaultKcpPort = 34320
 var DefaultTcpPort = 34320
 var DefaultTlsPort = 34321
 var DefaultUdpApiPort = 34321
 var DefaultKcpApiPort = 34322
+var DefaultGrpcPort = 34322
+
 var DefaultLoginKey = "HLLdsa544&*S"
 
 var DefaultHttpPort = 80
 var DefaultHttpsPort = 443
-
-var DefaultGrpcPort = 34322
