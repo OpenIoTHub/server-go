77889a7fb5fa419c8cd22b58d73e2df1e84d473e
iotserv <yu@iotserv.com>
2020-08-31 17:08:49 +0800

    update

--- delhistory.sh
+++ delhistory.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-git filter-branch --force --index-filter 'git rm --cached --ignore-unmatch */web-ui/' --prune-empty --tag-name-filter cat -- --all
+git filter-branch --force --index-filter 'git rm --cached -r --ignore-unmatch web-ui' --prune-empty --tag-name-filter cat -- --all
 git push origin master --force
